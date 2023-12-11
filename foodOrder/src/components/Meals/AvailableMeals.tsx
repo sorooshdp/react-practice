78c5f9870c94a57ed79a1f84143ef868c431de74
soroosh <sorooshdpki@gmail.com>
2023-12-12 02:56:34 +0330

    feat: add new componants to foodOrder project

--- foodOrder/src/components/Meals/AvailableMeals.tsx
+++ foodOrder/src/components/Meals/AvailableMeals.tsx
@@ -1,37 +1,48 @@
-import classes from './AvaliableMeals.module.css'
+import Card from "../UI/Card";
+import classes from "./AvaliableMeals.module.css";
+import MealItem from "./MealItem/MealItem";
 const DUMMY_MEALS = [
     {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
+        id: "m1",
+        name: "Sushi",
+        description: "Finest fish and veggies",
+        price: 22.99,
     },
     {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
+        id: "m2",
+        name: "Schnitzel",
+        description: "A german specialty!",
+        price: 16.5,
     },
     {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
+        id: "m3",
+        name: "Barbecue Burger",
+        description: "American, raw, meaty",
+        price: 12.99,
     },
     {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
+        id: "m4",
+        name: "Green Bowl",
+        description: "Healthy...and green...",
+        price: 18.99,
     },
-  ];
+];
 const AvaliableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>)
-    return <section className={classes.meals}>
-        <ul>
-            {mealsList}
-        </ul>
-    </section>
-}
+    const mealsList = DUMMY_MEALS.map((meal) => (
+        <MealItem
+            key={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+        />
+    ));
+    return (
+        <section className={classes.meals}>
+            <Card>
+                <ul>{mealsList}</ul>
+            </Card>
+        </section>
+    );
+};
 
-export default AvaliableMeals
+export default AvaliableMeals;
