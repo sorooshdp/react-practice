9731fdfa74fe4cab7575702e865de247f6b96bb4
soroosh <sorooshdpki@gmail.com>
2023-12-15 00:37:45 +0330

    feat: animation for header button and a more complex reducer function for provider

--- foodOrder/src/App.tsx
+++ foodOrder/src/App.tsx
@@ -1,5 +1,5 @@
 import { Fragment, useState } from 'react';
-
+import CartProvider from './store/CartProvider';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
@@ -16,13 +16,13 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
