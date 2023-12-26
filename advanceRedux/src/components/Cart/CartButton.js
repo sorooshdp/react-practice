1c3cb7cef11c8e0f32fbdc5a031ed94fcc0834ed
soroosh <sorooshdpki@gmail.com>
2023-12-26 23:34:14 +0330

    feat: add async fucntion

--- advanceRedux/src/components/Cart/CartButton.js
+++ advanceRedux/src/components/Cart/CartButton.js
@@ -5,7 +5,7 @@ import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
-  // const cartQuantity = useSelector((state) => state.cart.totalQuantity);
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
 
   const toggleCartHandler = () => {
     dispatch(uiActions.toggle());
@@ -14,7 +14,7 @@ const CartButton = (props) => {
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>7</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
