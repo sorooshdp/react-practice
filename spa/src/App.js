6145f94a5c12b9b10be0b53084db72eb2b54150c
soroosh <sorooshdpki@gmail.com>
2023-12-30 03:48:50 +0330

    feat: add productDetail component and relative paths

--- spa/src/App.js
+++ spa/src/App.js
@@ -7,6 +7,7 @@ import {
 
 import ErrorPage from './pages/Error';
 import HomePage from './pages/Home';
+import ProductDetailPage from './pages/ProductDetail';
 import ProductsPage from './pages/Products';
 import RootLayout from './pages/Root';
 
@@ -23,8 +24,9 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { path: '/', element: <HomePage /> },
-      { path: '/products', element: <ProductsPage /> },
+      { index: true, element: <HomePage /> },
+      { path: 'products', element: <ProductsPage /> },
+      { path: 'products/:productId', element: <ProductDetailPage /> }
     ],
   }
 ]);
