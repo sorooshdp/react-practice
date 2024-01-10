7cbe9bc33c7c39d1294ef501bd6178e9359755f2
soroosh <sorooshdpki@gmail.com>
2024-01-10 16:12:41 +0330

    feat: add routing to the next.js project

---
+++ Next.js/next-course/app/news/new-meetup/page.js
@@ -0,0 +1,14 @@
+// our-domain.com/new-meetup
+"use client";
+
+import NewMeetupForm from '../../../components/meetups/NewMeetupForm';
+
+function NewMeetupPage() {
+  function addMeetupHandler(enteredMeetupData) {
+    console.log(enteredMeetupData);
+  }
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />
+}
+
+export default NewMeetupPage;
